9193a0054789348bfd830df719e8c798ebcec784
simon <42590964+SimonAwiti@users.noreply.github.com>
2018-11-02 13:12:53 +0300

    [Finishes #161672861] running the app

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ def create_app(config):
     '''creating  the app'''
  
     app = Flask(__name__, instance_relative_config=True)
-    #app.config.from_object(configuration[config])
+    #app.config.from_object(configuration[config]) finish the configs
     app.secret_key = os.getenv("SECRET_KEY")
 
     return app
