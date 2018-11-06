dbc7c921787da858c77515818f36b995c08fc605
unknown <awitisimon23@gmail.com>
2018-11-06 15:16:12 +0300

    [feature #161705549] routes for getting and editing order status

---
+++ app/version1/parcel_status/views.py
@@ -0,0 +1,29 @@
+"""creating bp routes for the admin operations"""
+from flask import Blueprint, request, jsonify
+from app.version1.parcel_status.models import ParcelStatus
+from app.version1.parcel_delivery_order.validator import validate_parcel_data
+
+ParcelObject = ParcelStatus()
+
+
+version1pstatus_bp = Blueprint('version1pstatus_bp', __name__, url_prefix='/api/v1/admin_parcels')
+
+@version1pstatus_bp.route('/<string:sender_name>', methods=['GET'])
+def get_parcel_order_by_sender(sender_name, **kwargs):
+    """getting sale record by name of sender"""
+    data = ParcelObject.get_parcel_by_user(sender_name)
+    return data
+
+@version1pstatus_bp.route('/', methods=['GET'])
+def get_all_parcels_orders_by_client():
+    """getting all the parcel delivery orders"""
+    data = ParcelObject.get_all_parcels()
+    return jsonify({"message":data}), 201
+
+@version1pstatus_bp.route('/<int:order_id>', methods=['PUT'])
+def edit_parcel_by_client(order_id):
+    """change the status of the parcel"""
+    data = request.get_json()
+    status = data['status']
+    response = ParcelObject.update_parcel_order(order_id, status)
+    return response
