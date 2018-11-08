192380dd39f588df6c98888cf21d8454635a55ce
unknown <awitisimon23@gmail.com>
2018-11-08 04:46:48 +0300

    [chore #161705579] updated validators

--- app/version1/parcel_delivery_order/validator.py
+++ app/version1/parcel_delivery_order/validator.py
@@ -9,14 +9,10 @@ def validate_parcel_data(data):
             return "Description must be a string"
         elif data["quantity"] is False: 
             return "parcel quantity required"
-        elif data["quantity"] == "":
-            return "parcel quantity is required"
         elif isinstance(data["quantity"], int) is False:
             return "Quantity must be a number"
         elif data["price"] is False: 
             return "price required"
-        elif data["price"] == "":
-            return "price is required"
         elif data["price"] < 0:
             return "The minimum unit price must be above 0"
         elif isinstance(data["price"], int) is False:
