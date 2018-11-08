098c1969bd0ae72cd53e32c53ed606c242dfb2b7
unknown <awitisimon23@gmail.com>
2018-11-08 04:49:36 +0300

    [chore #161705579] parcel status bp structure

--- app/version1/parcel_status/views.py
+++ app/version1/parcel_status/views.py
@@ -8,17 +8,17 @@ ParcelObject = ParcelStatus()
 
 version1pstatus_bp = Blueprint('version1pstatus_bp', __name__, url_prefix='/api/v1/admin_parcels')
 
-@version1pstatus_bp.route('/<string:sender_name>', methods=['GET'])
-def get_parcel_order_by_sender(sender_name, **kwargs):
+@version1pstatus_bp.route('/<int:sender_id>', methods=['GET'])
+def get_parcel_order_by_sender(sender_id, **kwargs):
     """getting sale record by name of sender"""
-    data = ParcelObject.get_parcel_by_user(sender_name)
+    data = ParcelObject.get_parcel_by_user(sender_id)
     return data
 
 @version1pstatus_bp.route('/', methods=['GET'])
 def get_all_parcels_orders_by_client():
     """getting all the parcel delivery orders"""
     data = ParcelObject.get_all_parcels()
-    return jsonify({"message":data}), 201
+    return data
 
 @version1pstatus_bp.route('/<int:order_id>', methods=['PUT'])
 def edit_parcel_by_client(order_id):
@@ -26,4 +26,4 @@ def edit_parcel_by_client(order_id):
     data = request.get_json()
     status = data['status']
     response = ParcelObject.update_parcel_order(order_id, status)
-    return response
+    return jsonify ({"message": response})
